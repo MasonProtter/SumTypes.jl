e7e12cc7223a247aec5d6fde325bc28999cee860
Mason Protter <mason.protter@icloud.com>
2023-03-30 19:25:03 -0600

    switch to using bits for tags.

--- src/SumTypes.jl
+++ src/SumTypes.jl
@@ -12,6 +12,10 @@ function unwrap end
 function tags end
 function deparameterize end
 is_sumtype(::Type{T}) where {T}   = false
+function flagtype end
+function flag_to_symbol end
+function symbol_to_flag end
+function tags_flags_nt end
 
 
 struct Unsafe end
@@ -25,15 +29,19 @@ maybe_type(::Type{x}) where {x} = x
 maybe_type(::Singleton{x}) where {x} = Singleton{x}
 
 const tag = Symbol("#tag#")
-get_tag(x) =getfield(x, tag) 
+get_tag(x) =getfield(x, tag)
+get_tag_sym(x::T) where {T} = tags_flags_nt(T)[get_tag(x)]
+# get_tag(x::T) where {T} = getfield(x, tag)
+
 
 show_sumtype(io::IO, m::MIME, x) = show_sumtype(io, x)
-function show_sumtype(io::IO, x)
+function show_sumtype(io::IO, x::T) where {T}
     tag = get_tag(x)
-    if getfield(x, tag) isa Singleton
-        print(io, String(tag), "::", typeof(x))
+    sym = flag_to_symbol(T, tag)
+    if getfield(x, sym) isa Singleton
+        print(io, String(sym), "::", typeof(x))
     else
-        print(io, String(tag), '(', join((repr(data) for data ∈ getfield(x, tag)), ", "), ")::", typeof(x))
+        print(io, String(sym), '(', join((repr(data) for data ∈ getfield(x, sym)), ", "), ")::", typeof(x))
     end
 end
 
