a1c8a6f69f2cd5019f850b403d966559d59f104a
MasonProtter <mason.protter@icloud.com>
2023-04-02 20:45:16 -0600

    whoops, that killed performance

--- src/compute_storage.jl
+++ src/compute_storage.jl
@@ -80,8 +80,8 @@ make(::Type{ST}, to_make, tag) where {ST} = make(ST, to_make, tag, variants_Tupl
     con = Expr(
         :new,
         ST{bit_size, nptrs},
-        Expr(:tuple, ptr_args..., (nothing for _ ∈ 1:(nptrs-length(ptr_args)))...),
         :(unsafe_padded_reinterpret(NTuple{$bit_size, UInt8}, $bitvariant)),
+        Expr(:tuple, ptr_args..., (nothing for _ ∈ 1:(nptrs-length(ptr_args)))...),
         :tag
     )
 end
