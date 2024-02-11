7daecf19b910d5f01fe2ee0701fe96c0f88ba834
Tortar <adrianomeligrana1999@gmail.com>
2024-02-11 15:22:36 +0100

    format

--- src/precompile.jl
+++ src/precompile.jl
@@ -2,28 +2,28 @@
 using PrecompileTools
 
 @setup_workload begin
-    @compile_workload begin
-    	blk = :(begin 
-    				Bar(::Int) 
-    				Baz(x) 
-    			end)
-        _sum_type(:(Foo <: AbstractFoo), QuoteNode(:visible), blk)
-        blk = :(begin
-           			B{X}(a::X)
-          			C{X}(b::X)
-       			end)
-        _sum_type(:(A{X<:Union{Real, SumTypes.Uninit}}), QuoteNode(:visible), blk)
-        blk = :(begin
-				    A(common_field::Int, a::Bool, b::Int)
-				    B(common_field::Int, a::Int, b::Float64, d::Complex{Float64})
-				    C(common_field::Int, b::Float64, d::Bool, e::Float64, k::Complex{Float64})
-				    D(common_field::Int, b::Char)
-    			end)
-    	_sum_type(:(AT), QuoteNode(:visible), blk)
-    	blk = :(begin
-    				Left{A}(::A)
-    				Right{B}(::B)
+	@compile_workload begin
+		blk = :(begin 
+					Bar(::Int) 
+					Baz(x) 
 				end)
-    	_sum_type(:(Either2{A, B}), QuoteNode(:hidden), blk)
-    end
+		_sum_type(:(Foo <: AbstractFoo), QuoteNode(:visible), blk)
+		blk = :(begin
+		   			B{X}(a::X)
+		  			C{X}(b::X)
+	   			end)
+		_sum_type(:(A{X<:Union{Real, SumTypes.Uninit}}), QuoteNode(:visible), blk)
+		blk = :(begin
+					A(common_field::Int, a::Bool, b::Int)
+					B(common_field::Int, a::Int, b::Float64, d::Complex{Float64})
+					C(common_field::Int, b::Float64, d::Bool, e::Float64, k::Complex{Float64})
+					D(common_field::Int, b::Char)
+				end)
+		_sum_type(:(AT), QuoteNode(:visible), blk)
+		blk = :(begin
+					Left{A}(::A)
+					Right{B}(::B)
+				end)
+		_sum_type(:(Either2{A, B}), QuoteNode(:hidden), blk)
+	end
 end
